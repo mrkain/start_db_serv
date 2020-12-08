717c7612b16750fa0da5a4d44b46acb99a4b6b65
mrkain <leks.popow@gmail.com>
2020-12-08 15:34:04 +0300

    Трансформация данных API

--- src/services/swapi-service.js
+++ src/services/swapi-service.js
@@ -12,31 +12,79 @@ export  default  class SwapiService {
 
   async getAllPeople() {
     const res = await this.getResource(`/people/`)
-    return res.results
+    return res.results.map(this._transformPerson)
   }
-  getPerson(id) {
-    return this.getResource(`/people/${id}`)
+  async getPerson(id) {
+    const person = await this.getResource(`/people/${id}`)
+    return this._transformPerson(person)
   }
 
   async getAllPlanets() {
     const res = await this.getResource(`/planets/`)
-    return res.results
+    return res.results.map(this._transformPlanet)
   }
-  getPlanet(id) {
-    return this.getResource(`/planets/${id}`)
+  async getPlanet(id) {
+    const planet = await this.getResource(`/planets/${id}/`)
+    return this._transformPlanet(planet)
   }
 
   async getAllStarships() {
     const res = await this.getResource(`/starships/`)
-    return res.results
+    return res.results.map(this._transformStarship)
   }
   getStarship(id) {
-    return this.getResource(`/starships/${id}`)
+    const starship = this.getResource(`/starships/${id}`)
+    return this._transformStarship(starship)
   }
 
+  _extractId(item) {
+    const idRegExp = /\/([0-9]*)\/$/
+    return item.url.match(idRegExp)[1]
+  }
+
+  _transformPlanet(planet) {
+    return {
+      id: this._extractId(planet),
+      name:  planet.name,
+      population: planet.population,
+      rotationPeriod: planet.rotation_period,
+      diameter: planet.diameter,
+      orbitalPeriod: planet.orbital_period
+    }
+  }
+
+  _transformStarship(starship) {
+    return {
+      id: this._extractId(starship),
+      name: starship.name,
+      model: starship.model,
+      manufacturer: starship.manufacturer,
+      costInCredits: starship.costInCredits,
+      length: starship.length,
+      crew: starship.crew,
+      passengers: starship.passengers,
+      cargoCapacity: starship.cargoCapacity
+    }
+  }
+
+  _transformPerson(person) {
+    return {
+      id: this._extractId(person),
+      name: person.name,
+      gender: person.gender,
+      birthYear: person.birth,
+      eyeColor: person.eyeColor
+    }
+  }
 }
 
-const swapi = new SwapiService()
+
+
+
+
+
+
+/*const swapi = new SwapiService()
 
 swapi.getAllPeople().then((people) => {
   console.log('СПИСОК ЛЮДЕЙ')
@@ -72,8 +120,7 @@ swapi.getAllStarships().then((starships) => {
 swapi.getStarship(3).then((s) => {
   console.log('ТРЕТИЙ КОРАБЛЬ В СПИСКЕ')
   console.log(s.name)
-})
-
+})*/
 
 /*
 fetch('https://swapi.dev/api/people/1/')
